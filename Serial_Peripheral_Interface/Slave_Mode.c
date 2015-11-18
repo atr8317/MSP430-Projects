87a327c2e486b6690ffc34f5d73c32b543ff77a4
Amanda Reardon <atr8317@bu.edu>
2015-11-18 18:03:28 -0500

    Update Slave_Mode.c
    Changes made to comments for clarification

--- Serial_Peripheral_Interface/Slave_Mode.c
+++ Serial_Peripheral_Interface/Slave_Mode.c
@@ -2,7 +2,6 @@
  *
  * CONTROLLER B
  *
- *
 */
 
 #include "msp430g2553.h"
@@ -12,7 +11,7 @@
  void init_wdt(void);
 
 // Global variables and parameters (all volatilel to maintain for debugger)
-volatile unsigned int start = 0;			// flag for the start of a round
+volatile unsigned int start = 0;		// flag for the start of a round
 volatile unsigned int compare_rec = 0;		// 0 - no comparisons made yet
 volatile unsigned int game_over = 0;		// flags the end of the round
 volatile unsigned int guess = 0x800;		// start with the middle number of 65535
@@ -31,11 +30,11 @@ interrupt void WDT_interval_handler(){
 	//right now - setup to send one guess
 	if(start == 1 && game_over==0){
 		if(compare_rec == 0){
-			guess = 0x8000;					//first guess - 32768
-			UCB0TXBUF = guess>>8;			//transmit high order bits to master
-			//for(i = 10; i > 0; i--){		//to delay between transmissions
+			guess = 0x8000;			//first guess - 32768
+			UCB0TXBUF = guess>>8;		//transmit high order bits to master
+			//for(i = 10; i > 0; i--){	//to delay between transmissions
 			//}
-			UCB0TXBUF = guess;				//transmit low order bits to master
+			UCB0TXBUF = guess;		//transmit low order bits to master
 		}
 	}
 }
@@ -44,14 +43,14 @@ ISR_VECTOR(WDT_interval_handler, ".int10")
 void init_wdt(){
 	// setup the watchdog timer as an interval timer
 	// INTERRUPT NOT YET ENABLED!
-  	WDTCTL =(WDTPW +		// (bits 15-8) password
+  	WDTCTL =(WDTPW +	// (bits 15-8) password
      	                   	// bit 7=0 => watchdog timer on
        	                 	// bit 6=0 => NMI on rising edge (not used here)
                         	// bit 5=0 => RST/NMI pin does a reset (not used here)
            	 WDTTMSEL +     // (bit 4) select interval timer mode
-  		     WDTCNTCL  		// (bit 3) clear watchdog timer counter
-  		                	// bit 2=0 => SMCLK is the source
-  		     +2         	// bits 1-0 = 10=> source/512
+  		     WDTCNTCL  	// (bit 3) clear watchdog timer counter
+  		                // bit 2=0 => SMCLK is the source
+  		     +2         // bits 1-0 = 10=> source/512
  			 );
   	IE1 |= WDTIE; // enable WDT interrupt
  }
@@ -61,8 +60,8 @@ void init_wdt(){
 
 void interrupt spi_rx_handler(){
 	if(start==0){
-		start = UCB0RXBUF;	//
-		//for(i = 10; i > 0; i--){		//to delay between receiving bit streams
+		start = UCB0RXBUF;		//receive start flag from A
+		//for(i = 10; i > 0; i--){	//to delay between receiving bit streams
 		//}
 		//secret_number = secret_number + UCB0RXBUF;  // was used in debugging
 	}
@@ -82,18 +81,18 @@ ISR_VECTOR(spi_rx_handler, ".int07")
 
 void init_spi(){
 	UCB0CTL1 = UCSSEL_2+UCSWRST;  		// Reset state machine; SMCLK source;
-	UCB0CTL0 = UCCKPH					// Data capture on rising edge
-			   							// read data while clock high
-										// lsb first, 8 bit mode,
-										// SLAVE
-			   +UCMODE_0				// 3-pin SPI mode
-			   +UCSYNC;					// sync mode (needed for SPI or I2C)
-	UCB0BR0=BRLO;						// set divisor for bit rate
+	UCB0CTL0 = UCCKPH			// Data capture on rising edge
+			   			// read data while clock high
+						// lsb first, 8 bit mode,
+						// SLAVE
+			   +UCMODE_0		// 3-pin SPI mode
+			   +UCSYNC;		// sync mode (needed for SPI or I2C)
+	UCB0BR0=BRLO;				// set divisor for bit rate
 	UCB0BR1=BRHI;
-	UCB0CTL1 &= ~UCSWRST;				// enable UCB0 (must do this before setting
-										//              interrupt enable and flags)
-	IFG2 &= ~UCB0RXIFG;					// clear UCB0 RX flag
-	IE2 |= UCB0RXIE;					// enable UCB0 RX interrupt
+	UCB0CTL1 &= ~UCSWRST;			// enable UCB0 (must do this before setting
+						//              interrupt enable and flags)
+	IFG2 &= ~UCB0RXIFG;			// clear UCB0 RX flag
+	IE2 |= UCB0RXIE;			// enable UCB0 RX interrupt
 	// Connect I/O pins to UCB0 SPI
 	P1SEL |=SPI_CLK+SPI_SOMI+SPI_SIMO;
 	P1SEL2|=SPI_CLK+SPI_SOMI+SPI_SIMO;
@@ -109,7 +108,7 @@ void init_spi(){
 void main(){
 
 	WDTCTL = WDTPW + WDTHOLD;       // Stop watchdog timer
-	BCSCTL1 = CALBC1_8MHZ;			// 8Mhz calibration for clock
+	BCSCTL1 = CALBC1_8MHZ;		// 8Mhz calibration for clock
   	DCOCTL  = CALDCO_8MHZ;
 
   	init_spi();
